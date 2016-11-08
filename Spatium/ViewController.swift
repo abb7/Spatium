62c73474d886c0689fc98596d02b17b9b97933fe
Abb <Shamos@Abbs-MacBook-Pro.local>
2016-11-08 11:14:13 +0300

    Initial Commit

---
+++ Spatium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spatium
+//
+//  Created by Abb on 2/8/1438 AH.
+//  Copyright © 1438 Abb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
